d30a4c7b931587f6a5ec4b2856024dfa36c387be
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2023-02-12 15:22:48 +0000

    (feat) Added some types.

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+
+export const DIRECTIONS = ['N','E','S','W'];
+
+export const CAPABILITIES = ['operational','crashed'];
+
+export type point = {x: number, y: number};
+
+export type canvas = {origin: point, apex: point};
+
+export type rover = {
+    name: string;
+    location: point;
+    orientation: typeof DIRECTIONS[number];
+    condition: typeof CAPABILITIES[number];
+}
